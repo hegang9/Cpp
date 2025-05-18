22ed5ae00241ac13d69150eb4f96cc5ff4044178
HEGANG <hegang886@qq.com>
2025-05-18 17:59:41 +0800

    文件读写操作

--- Src/test.cpp
+++ Src/test.cpp
@@ -8,19 +8,23 @@ int main()
 {
 	//文件写
 	ofstream outFile;
-	outFile.open("C++Primer/文件读写操作.txt");
+	outFile.open("Note/文件读写操作.txt");
 	if (outFile.is_open()) {
-		outFile << "文件读写操作";
+		outFile << "文件读写操作" << endl;
 	}
 	else {
 		cout << "没有打开文件" << endl;
 	}
+	outFile.close();
+
 	//文件读
 	ifstream inFile;
-	inFile.open("C++Primer/文件读写操作.txt");
+	inFile.open("Note/文件读写操作.txt");
 	string in;
 	inFile >> in;
 	cout << in;
+
+	inFile.close();
 
 	return 0;
 }
